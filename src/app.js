b96435076089665b23c66661ea59d4d3a2ae6570
sbot19 <92232719+sbot19@users.noreply.github.com>
2021-10-15 20:41:26 -0400

    Updated the date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,45 @@
+function formatDate(timestamp) {
+  // Calculate the time and date
+
+  let date = new Date(timestamp);
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dayNumber = date.getDate();
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${month} ${dayNumber} | ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   //it's 'response' b/c the app gets a response from the API.
@@ -24,6 +66,10 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  //The '*1000' converts the milliseconds (from OpenWeatherMap) to actual Date and Time
 }
 
 let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
